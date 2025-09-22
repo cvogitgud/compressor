4c7318115166103f0bddc834ffb22831fe5b8dff
Chris <cvo13014@gmail.com>
2025-09-22 12:04:54 -0700

    added DSP modules

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -54,6 +54,9 @@ public:
     void setStateInformation (const void* data, int sizeInBytes) override;
 
 private:
+    
+    
+    
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (CompressorAudioProcessor)
 };
